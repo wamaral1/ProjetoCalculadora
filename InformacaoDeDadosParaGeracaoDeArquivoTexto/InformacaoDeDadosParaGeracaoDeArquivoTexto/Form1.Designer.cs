5cecd5fde41e02fac7e44b4a91c895a7805d7ca2
Wellington Amaral <Wellington.Amaral@edu.pucrs.br>
2023-03-26 23:28:35 -0400

    Ajustes 26/03/2023

---
+++ InformacaoDeDadosParaGeracaoDeArquivoTexto/InformacaoDeDadosParaGeracaoDeArquivoTexto/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace InformacaoDeDadosParaGeracaoDeArquivoTexto
+{
+    partial class FormGeracaoArquivoTexto
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.iniciarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sobreODesenvolvedorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtNumeroFuncionarios = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCriarLinhas = new System.Windows.Forms.Button();
+            this.btnCriarArquivo = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.dgvFuncionarios = new System.Windows.Forms.DataGridView();
+            this.sfdGravarArquivo = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.iniciarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(431, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // iniciarToolStripMenuItem
+            // 
+            this.iniciarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sobreODesenvolvedorToolStripMenuItem});
+            this.iniciarToolStripMenuItem.Name = "iniciarToolStripMenuItem";
+            this.iniciarToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.iniciarToolStripMenuItem.Text = "Iniciar";
+            // 
+            // sobreODesenvolvedorToolStripMenuItem
+            // 
+            this.sobreODesenvolvedorToolStripMenuItem.Name = "sobreODesenvolvedorToolStripMenuItem";
+            this.sobreODesenvolvedorToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.sobreODesenvolvedorToolStripMenuItem.Text = "Sobre o Desenvolvedor";
+            this.sobreODesenvolvedorToolStripMenuItem.Click += new System.EventHandler(this.sobreODesenvolvedorToolStripMenuItem_Click);
+            // 
+            // txtNumeroFuncionarios
+            // 
+            this.txtNumeroFuncionarios.Location = new System.Drawing.Point(119, 31);
+            this.txtNumeroFuncionarios.Name = "txtNumeroFuncionarios";
+            this.txtNumeroFuncionarios.Size = new System.Drawing.Size(58, 20);
+            this.txtNumeroFuncionarios.TabIndex = 1;
+            this.txtNumeroFuncionarios.TextChanged += new System.EventHandler(this.txtNumeroFuncionarios_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nr. de Funcionários:";
+            // 
+            // btnCriarLinhas
+            // 
+            this.btnCriarLinhas.Location = new System.Drawing.Point(183, 31);
+            this.btnCriarLinhas.Name = "btnCriarLinhas";
+            this.btnCriarLinhas.Size = new System.Drawing.Size(236, 20);
+            this.btnCriarLinhas.TabIndex = 3;
+            this.btnCriarLinhas.Text = "Criar linhas para registro";
+            this.btnCriarLinhas.UseVisualStyleBackColor = true;
+            this.btnCriarLinhas.Click += new System.EventHandler(this.btnCriarLinhas_Click);
+            // 
+            // btnCriarArquivo
+            // 
+            this.btnCriarArquivo.Location = new System.Drawing.Point(16, 341);
+            this.btnCriarArquivo.Name = "btnCriarArquivo";
+            this.btnCriarArquivo.Size = new System.Drawing.Size(232, 20);
+            this.btnCriarArquivo.TabIndex = 4;
+            this.btnCriarArquivo.Text = "Criar Arquivo";
+            this.btnCriarArquivo.UseVisualStyleBackColor = true;
+            this.btnCriarArquivo.Click += new System.EventHandler(this.btnCriarArquivo_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(254, 341);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(165, 20);
+            this.btnReiniciar.TabIndex = 5;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // dgvFuncionarios
+            // 
+            this.dgvFuncionarios.AllowUserToAddRows = false;
+            this.dgvFuncionarios.AllowUserToOrderColumns = true;
+            this.dgvFuncionarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFuncionarios.Location = new System.Drawing.Point(16, 57);
+            this.dgvFuncionarios.Name = "dgvFuncionarios";
+            this.dgvFuncionarios.Size = new System.Drawing.Size(403, 278);
+            this.dgvFuncionarios.TabIndex = 6;
+            // 
+            // sfdGravarArquivo
+            // 
+            this.sfdGravarArquivo.DefaultExt = "*.txt";
+            this.sfdGravarArquivo.Filter = "Arquivos textos|*.txt";
+            this.sfdGravarArquivo.Title = "Dados para geração de arquivo";
+            // 
+            // FormGeracaoArquivoTexto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 373);
+            this.Controls.Add(this.dgvFuncionarios);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnCriarArquivo);
+            this.Controls.Add(this.btnCriarLinhas);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumeroFuncionarios);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormGeracaoArquivoTexto";
+            this.Text = "Dados para geração de arquivos";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem iniciarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sobreODesenvolvedorToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtNumeroFuncionarios;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCriarLinhas;
+        private System.Windows.Forms.Button btnCriarArquivo;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.DataGridView dgvFuncionarios;
+        private System.Windows.Forms.SaveFileDialog sfdGravarArquivo;
+    }
+}
+
---
+++ InformacaoDeDadosParaGeracaoDeArquivoTexto/InformacaoDeDadosParaGeracaoDeArquivoTexto/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
+
+namespace InformacaoDeDadosParaGeracaoDeArquivoTexto
+{
+    public partial class FormGeracaoArquivoTexto : Form
+    {
+        public FormGeracaoArquivoTexto()
+        {
+            InitializeComponent();
+            dgvFuncionarios.ColumnCount = 2;
+            dgvFuncionarios.Columns[0].HeaderText =
+            "Nome";
+            dgvFuncionarios.Columns[0].Width = 230;
+            dgvFuncionarios.Columns[1].HeaderText =
+            "Salário";
+            dgvFuncionarios.Columns[1].Width = 67;
+        }
+
+        private void sobreODesenvolvedorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Este programa foi desvenvolvido por " +
+                "Wellington Amaral utilizando C# em 03/2023.", "Sobre o " +
+                "Desenvolvedor", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+        }
+
+        private void btnCriarLinhas_Click(object sender, EventArgs e)
+        {
+            int numeroFuncionarios = Convert.ToInt16(
+            txtNumeroFuncionarios.Text);
+            if (numeroFuncionarios < 1)
+                numeroFuncionarios = 1;
+            int i = 0;
+            do
+            {
+                var linhaTabela = new DataGridViewRow();
+                linhaTabela.Cells.Add(new DataGridViewTextBoxCell
+                {
+                    Value = string.Empty
+                });
+                linhaTabela.Cells.Add(new
+                DataGridViewTextBoxCell
+                { Value = 0 });
+                dgvFuncionarios.Rows.Add(linhaTabela);
+            } while (++i < numeroFuncionarios);
+            txtNumeroFuncionarios.Enabled = false;
+            btnCriarArquivo.Enabled = true;
+            btnReiniciar.Enabled = true;
+            btnCriarLinhas.Enabled = false;
+        }
+
+        private void btnReiniciar_Click(object sender, EventArgs e)
+        {
+            dgvFuncionarios.Rows.Clear();
+            txtNumeroFuncionarios.Text = string.Empty;
+            txtNumeroFuncionarios.Enabled = true;
+            btnCriarArquivo.Enabled = false;
+            btnReiniciar.Enabled = false;
+            btnCriarLinhas.Enabled = true;
+        }
+
+        private void btnCriarArquivo_Click(object sender, EventArgs e)
+        {
+            if (!ValidaDados())
+            {
+                MessageBox.Show("Os dados possuem " +
+                "problemas.Verifique se não deixou " +
+                "nenhum nome em branco ou se existe um " + 
+                "valor correto para os salários de cada "+
+                "um");
+            }
+            else if (sfdGravarArquivo.ShowDialog() ==
+            DialogResult.OK)
+            {
+                GerarArquivo();
+                MessageBox.Show("Arquivo gerado com " +
+                "sucesso");
+
+            }
+        }
+
+        private void GerarArquivo()
+        {
+            StreamWriter wr = new StreamWriter(
+            sfdGravarArquivo.FileName, true);
+            for (int j = 0;
+            j < dgvFuncionarios.Rows.Count;
+            j++)
+            {
+                wr.WriteLine(dgvFuncionarios.Rows[j].
+                Cells[0].Value.ToString() + ";" +
+                dgvFuncionarios.Rows[j].Cells[1].
+                Value.ToString());
+            }
+            wr.Close();
+        }
+        private bool ValidaDados()
+        {
+            int i = 0;
+            bool dadosValidados = true;
+            double stringToDouble;
+            do
+            {
+                if (string.IsNullOrWhiteSpace(
+                dgvFuncionarios.Rows[i].Cells[0].Value.
+                ToString()))
+                    dadosValidados = false;
+                if (!Double.TryParse(dgvFuncionarios.
+                Rows[i].Cells[1].Value.ToString(),
+                out stringToDouble))
+                    dadosValidados = false;
+            } while (++i < dgvFuncionarios.Rows.Count);
+            return dadosValidados;
+        }
+
+        private void txtNumeroFuncionarios_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
